115069c5dfd344b36f5400361f6fc6702ace9a68
Alireza <arnn379@gmail.com>
2022-11-28 01:50:02 +0330

    Last commit for CA2

--- src/test/java/StudentControllerTest.java
+++ src/test/java/StudentControllerTest.java
@@ -1,8 +1,11 @@
 import com.google.common.collect.Iterables;
+import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.controller.student.StudentController;
 import ir.proprog.enrollassist.controller.student.StudentView;
+import ir.proprog.enrollassist.domain.section.Section;
 import ir.proprog.enrollassist.domain.student.Student;
 import ir.proprog.enrollassist.domain.student.StudentNumber;
+import ir.proprog.enrollassist.domain.user.User;
 import ir.proprog.enrollassist.repository.*;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -15,11 +18,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.hamcrest.Matchers.samePropertyValuesAs;
-import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class StudentControllerTest {
@@ -37,6 +37,10 @@ public class StudentControllerTest {
     @InjectMocks
     private StudentController controller;
 
+
+    // studentRepository:stub, arman:dummy, rohollah:dummy, arsham:dummy
+    // State Verification
+    // Mockisty
     @Test
     public void allTest() {
         Student arman = mock(Student.class);
@@ -47,15 +51,46 @@ public class StudentControllerTest {
         Assert.assertEquals(3, Iterables.size(controller.all()));
     }
 
+    // studentRepository:stub, arman:dummy
+    // State Verification
+    // Mockisty
     @Test
     public void oneTest() {
         Student arman = mock(Student.class);
         when(studentRepository.findByStudentNumber(any(StudentNumber.class))).thenReturn(java.util.Optional.ofNullable(arman));
-        var temp = new StudentView(arman);
-        var temp2 = controller.one("81019598");
-//        Assert.assertEquals(new StudentView(arman), controller.one("810197598"));
-        assertThat(controller.one("81019598"), samePropertyValuesAs(new StudentView(arman)));
-        Assert.assertReflectionEquals(user1, user2);
+        Assert.assertEquals(new StudentView(arman).getStudentId(), controller.one("810197598").getStudentId());
+    }
+
+    // user:stub, studentRepository:stub, userRepository:stub
+    // State Verification
+    // Classical
+    @Test
+    public void addStudentTest() throws ExceptionList {
+        User user = mock(User.class);
+        Student student = new Student("810198533", "Undergraduate");
+        StudentView studentView = new StudentView(student);
+        when(user.getUserId()).thenReturn("1234");
+        when(studentRepository.findByStudentNumber(any(StudentNumber.class))).thenReturn(java.util.Optional.ofNullable(null));
+        when(userRepository.findByUserId(studentView.getUserId())).thenReturn(java.util.Optional.ofNullable(user));
+        when(studentRepository.save(any(Student.class))).thenReturn(null);
+        when(userRepository.save(any(User.class))).thenReturn(null);
+
+        Assert.assertEquals(studentView.getStudentNo(), controller.addStudent(studentView).getStudentNo());
+    }
+
+    // studentRepository:stub, sec1:dummy, sec2:dummy, sec3:dummy
+    // State Verification
+    // Classical
+    @Test
+    public void findTakeableSectionsByMajorTest() throws ExceptionList {
+        Section sec1 = mock(Section.class);
+        Section sec2 = mock(Section.class);
+        Section sec3 = mock(Section.class);
+        Student student = new Student("810198533", "Undergraduate");
+        when(studentRepository.findByStudentNumber(any(StudentNumber.class))).thenReturn(java.util.Optional.ofNullable(student));
+        when(sectionRepository.findAll()).thenReturn(Arrays.asList(sec1, sec2, sec3));
+
+        Assert.assertEquals(0, 1);
     }
 
 }
